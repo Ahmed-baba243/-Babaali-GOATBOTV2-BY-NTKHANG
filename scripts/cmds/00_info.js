17dc711e232f7abf0d65d4527c2ccff4f524a25c
Ahmed-baba243 <akanahairi710@gmail.com>
2024-05-28 06:43:12 +0000

    Update 00_info.js

--- scripts/cmds/00_info.js
+++ scripts/cmds/00_info.js
@@ -15,14 +15,14 @@ module.exports = {
 		envConfig: {}
 	},
 	onStart: async function ({ message }) {
-		const botName = "yazky";
-		const botPrefix = "!";
-		const authorName = "CliffVincent";
-		const ownAge = "18";
+		const botName = "Heaven";
+		const botPrefix = "-";
+		const authorName = "Heaven contro";
+		const ownAge = "17";
 		const teamName = "Github team";
-		const authorFB = "https://www.facebook.com/swordigo.swordslush";
+		const authorFB = " https://www.facebook.com/profile.php?id=61557094816783";
 		const authorInsta = "Thead469";
-		const tikTok = "tiktok.com/@Cliffthegreat0";
+		const tikTok = "tiktok.com/@Heaventhegreat0";
 		const urls = JSON.parse(fs.readFileSync('cliff.json'));
 		const link = urls[Math.floor(Math.random() * urls.length)];
 		const now = moment().tz('Asia/Jakarta');
