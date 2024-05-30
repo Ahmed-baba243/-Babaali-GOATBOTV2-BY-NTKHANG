04c7c2214a40914656037248d1f99c474382080c
Ahmed-baba243 <akanahairi710@gmail.com>
2024-05-30 05:11:42 +0000

    Create ping2.js

---
+++ scripts/cmds/ping2.js
@@ -0,0 +1,32 @@
+module.exports = {
+    config: {
+       name: 'ping2',
+       version: '5.8',
+       role: 0,
+       category: 'SySteM',
+       longDescription: 'Show the bot ping or status',
+       author: 'UPoL',
+       guide: {
+         en: '{p}{n}'
+        },
+    },
+    onStart: async function ({ api, event, message }) {
+         const timeStart = Date.now();
+         await message.reply('Loading....!');
+         const ping = Date.now() - timeStart;
+         let pingStatus = " 🟢 | Very Good ";
+    if (ping > 200) {
+      pingStatus = '🫠 | Good..';
+    }
+    if (ping > 500) {
+      pingStatus = '✅ | Medium..!!';
+    }
+    if (ping > 1000) {
+      pingStatus = '👀 | Net slow...';
+    }
+    if (ping > 1500) {
+      pingStatus = '⚠ | Bad.!';
+    }
+         message.reply(`===== PING STATUS =====\n\nPong: ${ping}\nStatus: ${pingStatus}`);
+     }
+  };
